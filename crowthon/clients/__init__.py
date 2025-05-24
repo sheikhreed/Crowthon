2f8b9393f09ac24597126158bef3b4d50bdf4a15
sheikhreed <sheikhreed@protonmail.com>
2025-05-24 14:13:37 +0600

    Add MongoDB integration

--- crowthon/clients/__init__.py
+++ crowthon/clients/__init__.py
@@ -1,2 +1,5 @@
 # Import the user client, bot client, and bot token
 from .telegram import crowthon, crowthon_bot, token
+
+# Imports the 'mongodb' object from the 'mongo' module 
+from .mongo import mongodb
